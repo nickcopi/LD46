a8f44b9fc243d9b682508aa2411ca401d5316b56
nick <nick@noemail4unonon.comlol>
2020-04-19 15:00:04 -0400

    Split the grid making out into its own class for reusability.

--- InGame.js
+++ InGame.js
@@ -1,97 +1,15 @@
 class InGame extends Scene{
 	constructor(canvas,exit,extra){
 		super(canvas,exit);
-		this.grid = [];
 		this.gridSize = 26;
-		this.player = new Player(0,0,this.gridSize,this.gridSize);
 		this.seed = extra.seed;
-		this.random = new Random(this.seed);
-		this.initGrid();
+		this.gridMaker = new GridMaker(this.seed,this.gridSize);
+		this.grid = this.gridMaker.grid;
+		this.player = new Player(this.gridMaker.spawnX,this.gridMaker.spawnY,this.gridSize,this.gridSize);
 		this.initControls();
 		this.won = false;
 		this.tick = 0;
 		this.save = new Save();
-	}
-	initGrid(){
-		for(let i = 0; i < this.gridSize; i++){
-			const newArray = [];
-			for(let j = 0; j < this.gridSize; j++){
-				let type = this.random.next()>0.5?GridEnum.LOCKED:GridEnum.OPEN;
-				//if(j === 0 || i === 0 || j === this.gridSize-1 || i === this.gridSize-1) type = GridEnum.LOCKED;
-				newArray.push(new GridItem(i*this.gridSize,j*this.gridSize,this.gridSize,type));
-			}
-			this.grid.push(newArray);
-		}
-		this.findNeighbors();
-		//for(let i = 0; i < 200; i++){
-		//	this.findNeighbors();
-		//	for(let y = 0; y < this.gridSize; y++){
-		//		for(let x = 0; x < this.gridSize; x++){
-		//			const item = this.grid[y][x];
-		//			if(item.openNeighbors < 1)
-		//				item.type = GridEnum.LOCKED;
-		//		}
-		//	}
-		//}
-		let flow = 1;
-		let flows = [];
-		for(let y = 0; y < this.gridSize; y++){
-			for(let x = 0; x < this.gridSize; x++){
-				const gridItem = this.grid[y][x];
-				if(gridItem.type === GridEnum.LOCKED) continue;
-				if(gridItem.flow) continue;
-				let openSet = [gridItem];
-				while(openSet.length > 0){
-					const item = openSet.pop();
-					if(item.flow || item.type === GridEnum.LOCKED) continue;
-					item.flow = flow;
-					if(flows[flow]) flows[flow]++;
-					else flows[flow] = 1;
-					openSet = openSet.concat(item.neighbors);
-				}
-				flow++;
-			}
-		}
-		let maxFlow = flows[1];
-		let maxFlowIndex = 1;
-		for(let i = 2; i < flows.length;i++){
-			if(flows[i] > maxFlow){
-				maxFlow = flows[i];
-				maxFlowIndex = i;
-			}
-		}
-		for(let y = 0; y < this.gridSize; y++){
-			for(let x = 0; x < this.gridSize; x++){
-				const gridItem = this.grid[y][x];
-				if(gridItem.flow !== maxFlowIndex) gridItem.type = GridEnum.LOCKED;
-				else {
-					this.player.y = this.gridSize * x;
-					this.player.x = this.gridSize * y;
-				}
-			}
-		}
-	}
-	findNeighbors(){
-		for(let y = 0; y < this.gridSize; y++){
-			for(let x = 0; x < this.gridSize; x++){
-				const gridItem = this.grid[y][x];
-				gridItem.neighbors = [];
-				if(x > 0)
-					gridItem.neighbors.push(this.grid[y][x-1]);
-				if(y > 0)
-					gridItem.neighbors.push(this.grid[y-1][x]);
-				if(x < this.gridSize-1)
-					gridItem.neighbors.push(this.grid[y][x+1]);
-				if(y < this.gridSize-1)
-					gridItem.neighbors.push(this.grid[y+1][x]);
-				let open = 0;
-				gridItem.neighbors.forEach(neighbor=>{
-					if(neighbor.type !== GridEnum.LOCKED)
-						open++;
-				});
-				gridItem.openNeighbors = open;
-			}
-		}
 	}
 	paintGrid(){
 		this.grid[this.player.gridX(this.gridSize)][this.player.gridY(this.gridSize)].type = GridEnum.PAINTED;
